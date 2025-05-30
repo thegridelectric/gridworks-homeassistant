d383a619ae4c583aabca8b82cabf66f4873f9111
thdfw <th.defauw@gmail.com>
2025-05-30 22:14:27 +0200

    Undo Remove setup form when installig from HACS

--- custom_components/gridworks_api/config_flow.py
+++ custom_components/gridworks_api/config_flow.py
@@ -3,14 +3,14 @@ import voluptuous as vol
 
 from .const import DOMAIN
 
-class MyAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for the My API integration."""
+class GridWorksAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for the GridWorks API integration."""
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
             # Save config entry
-            return self.async_create_entry(title="My API", data=user_input)
+            return self.async_create_entry(title="GridWorks API", data=user_input)
 
         # Show setup form
         return self.async_show_form(
