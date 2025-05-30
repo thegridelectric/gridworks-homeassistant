b0a9e15b6bbd39f46c73a35ad975ff0c1999f94b
thdfw <th.defauw@gmail.com>
2025-05-30 18:45:58 +0200

    Debug

--- custom_components/my_api/__init__.py
+++ custom_components/my_api/__init__.py
@@ -1,6 +1,7 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 from .api import ApiTankModule
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.http.register_view(ApiTankModule(hass))
     return True
